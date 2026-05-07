3a24713f5e0fd505f53ac1ba1105aa106f5d39d6
Tejal <tejalpatil2002@gmail.com>
2026-05-07 15:16:52 +0530

    second commit

--- src/components/App.js
+++ src/components/App.js
@@ -7,13 +7,13 @@ import PageLayout from "./PageLayout";
 const App = () => {
 
   let Header=<h1>Welcome to my website</h1>;
-  let Footer=<p>2023 My Website.All rights reserved</p>;
+  let Footer=<p>©️2023 My Website.All rights reserved</p>;
 
   return (
 
     <div>
         <PageLayout Header={Header} Footer={Footer}>
-          <p>©️ This is my content of my website </p>
+          <p>This is my content of my website </p>
         </PageLayout>
     </div>
   )
